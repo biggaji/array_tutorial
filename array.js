5dd3bbcc56bd10caa81f8d3db14c210030d6417b
Tobi Ajibade <tobiajibade2017@yandex.com>
2021-06-14 13:33:48 +0100

    removed 6 fromm ages array

--- array.js
+++ array.js
@@ -12,7 +12,7 @@ let str = doublePrices.forEach(e => {
 
 // filter test for values passing a test, returns a new array of values that passes
 
-let ages = [18, 23, 26, 5,8, 7, 4];
+let ages = [18, 23, 26, 5,8, 7,6];
 
 let underEighteen = ages.filter((age) => {
     return age < 18;
